4b663d970b110128c1696c29d93616cf8f129ca8
nguyenthanhluong <luong7002351@gmail.com>
2023-09-28 21:06:38 +0700

    upheader

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,36 @@ module.exports = {
     extend: {
       colors: {
         "color-main": "#cc005e",
+        "color-light": "#ffffff",
+        "color-gray": "#f6f7fb",
+        "color-pink": "rgb(255, 102, 160)",
         black: {
           100: "#1A202C",
         },
         blue: {
           500: "#2975FF",
         },
+      },
+      maxWidth: {
+        pc: "1200px",
+      },
+      borderColor: {
+        colorGray: "#f6f7fb",
+      },
+      keyframes: {
+        showInput: {
+          from: {
+            transform: " translateX(50%)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateX(0)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        showInput: "showInput .3s linear",
       },
     },
   },
