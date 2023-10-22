18245ada4d7e942bacc25b32f0ae3671cfb0e88c
nguyenthanhluong <luong7002351@gmail.com>
2023-10-22 23:04:23 +0700

    aa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    HTTP_URL: "http://xuantuyen1207.website",
+    HTTP_URL: "https://xuantuyen1207.website",
     HTTPS_URL: "https://xuantuyen1207.website",
     NEXTAUTH_SECRET: "lsdkmlskdmflksdkskmsdnkj",
     URL: "http://localhost:3000/",
