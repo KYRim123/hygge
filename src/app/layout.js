a2daea3c56fdaf58c02be849042e5ef1149d63e6
Xuan Tuyen <dinhxuantuyen1207@gmail.com>
2023-10-27 07:32:51 +0700

    feat/test_chat_v2

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Wrapper from "./components/Wrapper";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
 import Providers from "./components/Provider";
+import Script from "next/script";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -14,11 +15,35 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
-        <Providers>
-          <Wrapper>{children}</Wrapper>
-          <Toaster />
-        </Providers>
+      <head>{/* Add any global head elements here */}</head>
+      <body>
+        {/* <!-- Messenger Chat plugin Code --> */}
+        <div id="fb-root"></div>
+
+        {/* <!-- Your Chat plugin code --> */}
+        <div className="fb-customerchat"></div>
+        <Script
+          id="messenger-tag"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `{Code within first script tag}`,
+          }}
+        ></Script>
+        <Script
+          id="messenger-sdk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `{Code within second script tag}`,
+          }}
+        ></Script>
+        <div className="montserrat">
+          {" "}
+          {/* Assuming 'montserrat' is a CSS class */}
+          <Providers>
+            <Wrapper>{children}</Wrapper>
+          </Providers>
+        </div>
+        <Toaster />
       </body>
     </html>
   );
