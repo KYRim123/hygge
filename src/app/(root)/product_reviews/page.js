e7eef6da0cc7acc4fa64b7172634e9aac7bf37c1
nguyenthanhluong <luong7002351@gmail.com>
2023-11-15 23:37:54 +0700

    css-purchase

--- src/app/(root)/product_reviews/page.js
+++ src/app/(root)/product_reviews/page.js
@@ -3,9 +3,7 @@
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import axios from "axios";
-import Image from "next/image";
-import style from "./index.module.css";
-import Link from "next/link";
+import ItemPurchase from "@/app/components/ItemPurchase";
 
 const ProductReviews = () => {
   const tabAll = 0;
@@ -53,46 +51,16 @@ const ProductReviews = () => {
             </div>
           ))}
       </div>
-      <div className="mt-2 h-[500px]">
+      <div className=" mt-2 h-[500px]">
         <div className="h-full overflow-y-scroll">
-          <div>
+          <div className="flex flex-col gap-3 p-3">
             {data
               ?.filter((item) => (crTab == 0 ? true : tabs[crTab].status.includes(item.id_trang_thai)))
               .map((item_child, index) => (
-                <div
-                  key={index}
-                  className={style.body_order}
-                >
-                  <div className="text-teal-400">Code Orders : {item_child.id}</div>
-
-                  <div className="ml-10">
-                    {item_child?.chi_tiet_hoa_don?.map((item_chi_tiet, key_chi_tiet) => (
-                      <div
-                        key={key_chi_tiet}
-                        className="flex"
-                      >
-                        <Image
-                          src={`${process.env.HTTPS_URL}/upload/${item_chi_tiet?.san_pham?.hinh_anh[0]?.hinh_anh_san_pham}`}
-                          width={50}
-                          height={50}
-                          alt="imageSlide"
-                          style={{ objectFit: "cover" }}
-                          priority={true}
-                        />
-                        <div className="flex items-center">
-                          <div className="ml-10">Name Item : {item_chi_tiet?.san_pham?.ten_san_pham}</div>
-                          <div className="ml-10">Quantity : {item_chi_tiet?.so_luong}</div>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                  <Link href={`/bill/status/${item_child.id}`}>
-                    <div className="ml-10 underline text-teal-400">View Status</div>
-                  </Link>
-                  <Link href={`/invoice/${item_child.id}`}>
-                    <div className="ml-10 underline text-teal-400">View Invoice</div>
-                  </Link>
-                </div>
+                <ItemPurchase
+                  index={index}
+                  item_child={item_child}
+                />
               ))}
           </div>
         </div>
