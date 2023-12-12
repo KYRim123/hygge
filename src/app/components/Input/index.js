edf4b61983bfae5da50b3f8f5291247c8e451bdd
nguyenthanhluong <luong7002351@gmail.com>
2023-12-12 20:16:01 +0700

    fix-pw

--- src/app/components/Input/index.js
+++ src/app/components/Input/index.js
@@ -4,7 +4,7 @@ import { NAME_TK, NAME_TND, NAME_EMAIL, NAME_MK, NAME_DC, NAME_SDT } from "@/app
 
 const Input = (props) => {
   const { label, value, name, errorMessage, onChange, width, ...inputProps } = props;
-  const [showError, setShowError] = useState();
+  const [showError, setShowError] = useState(false);
 
   function validateInput(name, value) {
     switch (name) {
@@ -32,12 +32,17 @@ const Input = (props) => {
         const re1 = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
         return re1.test(value).toString();
       }
+      case "newPassword": {
+        const re1 = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/;
+        return re1.test(value).toString();
+      }
       default: {
-        return value === "" ? "true" : "false";
+        return value === "" ? "false" : "true";
       }
     }
   }
 
+ 
   return (
     <div className={`${styles.formInput} flex flex-col flex-grow ${width}`}>
       <label htmlFor="email">{label}</label>
@@ -46,8 +51,8 @@ const Input = (props) => {
         value={value}
         className="outline-main-100 border-2 border-gray-200 py-3 px-4  rounded-full text-xl"
         onChange={onChange}
-        onBlur={() => setShowError(!showError)}
-        show={showError ? validateInput(name, value) : false}
+        onBlur={() => setShowError(true)}
+        show={showError ? validateInput(name, value) : true}
         {...inputProps}
       />
       <span className="text-red-500">{errorMessage}</span>
