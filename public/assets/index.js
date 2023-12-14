16d8b7b8676f1d0402c683661535240acd9bfd06
nguyenthanhluong <luong7002351@gmail.com>
2023-12-14 15:59:49 +0700

    done-page-about-fix

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,13 +1,14 @@
 // images
 const avaReview1 = require("./image/ava-1.jpg");
 const chuky = require("./image/chuky.png");
+const giayChungNhan = require("./image/giay_chung_nhan.jpg");
 
 const listImageDetails = [
   require("./image/card-pic-1.png"),
   require("./image/card-pic-2.png"),
   require("./image/card-pic-3.png"),
 ];
-export { avaReview1, chuky, listImageDetails };
+export { avaReview1, chuky, listImageDetails, giayChungNhan };
 // icons
 const iconLogo = require("./icons/logo.png");
 
